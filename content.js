bb25fe833738cf21bbe6d732414d3c6b0759c8a1
Yerlan <yerlan.temir@btsdigital.kz>
2020-04-14 20:56:19 +0500

    Add functionality for dynamically changing of likes/dislikes/ratio with MutationObserver

--- content.js
+++ content.js
@@ -1,69 +1,160 @@
 
 
 
+
+
 window.addEventListener('load', main, false);
 
-divs = undefined
+
+const observer = new MutationObserver(function (mutations) {
+
+    divs = document.querySelector(".table-responsive.question-list-table");
+    var arr = Array.from(divs.getElementsByTagName("tr"))
+
+    
+    get_likes_from_db(arr).then((likes)=>{
+        
+        like_statistics = likes['like_statistics'];
+        question2_index = likes['question2_index'];
+        
+        for(let [key,value] of Object.entries(like_statistics)){
+
+            ratio_p = question2_index[key].getElementsByClassName("ratio_value")[0];
+            
+            if(ratio_p == null){
+                
+                tds_to_add = get_likes_dislikes_dom_element(value);
+                
+                for(let td of tds_to_add)
+                    question2_index[key].appendChild(td);
+                
+                continue;
+
+            }
+            ratio_p.innerHTML = value['ratio'];
+
+            like_p = question2_index[key].getElementsByClassName("like_value")[0];
+            like_p.innerHTML = value['like'];
+    
+            dislike_p = question2_index[key].getElementsByClassName("dislike_value")[0];
+            dislike_p.innerHTML = value['dislike'];
+
+        }
+    });
+
+})
+
+
 function main (evt) {
     
     var jsInitChecktimer = setInterval(checkForJS_Finish, 222);
 
+
+
     function checkForJS_Finish () {
-        divs = document.getElementsByClassName("table-responsive question-list-table");
         
-        if (typeof(divs[0]) != 'undefined')
-         {
-            clearInterval (jsInitChecktimer);
+        filter_to_observ = document.querySelector(".filter-selected-tags.col-xs-12");
+    
+        divs = document.querySelector(".table-responsive.question-list-table");
+    
+        if (typeof(divs) != 'undefined' & divs != null)
+        {
 
-            var arr = Array.from(divs[0].getElementsByTagName("tr"))
-            arr[0].appendChild(get_additional_button("Likes"));
-            arr[0].appendChild(get_additional_button("Dislikes"));
-            arr[0].appendChild(get_additional_button("Ratio"));
-
-            questions_list = {}
-
-            request_url = "http://127.0.0.1:5000/v1/get_data/?questions_index="
+            observer.observe(filter_to_observ, {
+                subtree:true,
+                childList: true,
+                characterData:true
+              })
             
-            for(let question of arr.slice(1,-1)){
-                tds = question.getElementsByTagName("td");
-                question_index = tds[1].innerHTML;
-                request_url += question_index + ','
-                
-                questions_list[question_index] = question
-            }
+            clearInterval (jsInitChecktimer);
+           
+           
+            var arr = Array.from(divs.getElementsByTagName("tr"));
+            menu_row = arr[0];
+            
+            menu_row.appendChild(get_additional_button("Likes"));
+            menu_row.appendChild(get_additional_button("Dislikes"));
+            menu_row.appendChild(get_additional_button("Ratio"));
+            
 
-            const Http = new XMLHttpRequest();
-            Http.open("GET", request_url);
-            Http.send();
+            get_likes_from_db(arr).then((likes)=>{
+                like_statistics = likes['like_statistics'];
+                question2_index = likes['question2_index'];
 
-            Http.onreadystatechange = (e) => {
-                if(Http.readyState == XMLHttpRequest.DONE){
-                    var status = Http.status;
-                    if (status === 0 || (status >= 200 && status < 400)) {
-                      // The request has been completed successfully
-                        likes_statistics = JSON.parse(Http.responseText);
-                        for(let [key,value] of Object.entries(likes_statistics)){
-                            questions_list[key].appendChild(get_likes_dislikes_dom_element(value))
-                        }
-                    } else {
-                      // Oh no! There has been an error with the request!
-                    }
+                for(let [key,value] of Object.entries(like_statistics)){
+                    tds_to_add = get_likes_dislikes_dom_element(value);
+                    for(let td of tds_to_add)
+                        question2_index[key].appendChild(td);
                 }
-            }
+            });
+            
         }
     }
 
 }
-function get_likes_dislikes_dom_element(values){
-    td = document.createElement('td');
+
+function get_likes_from_db(arr){
+
+    return new Promise(function(resolve,reject){
+        
+        question2_index = {};
+
+        request_url = "http://127.0.0.1:5000/v1/get_data/?questions_index=";
+        
+        
+        for(let question of arr.slice(1,-1)){
+            tds = question.getElementsByTagName("td");
+            question_index = tds[1].innerHTML;
+            request_url += question_index + ',';
+            question2_index[question_index] = question;
+        }
+        
+        const Http = new XMLHttpRequest();
+        Http.open("GET", request_url);
+        Http.send();
+
+
+        Http.onreadystatechange = (e) => {
+
+            if(Http.readyState == XMLHttpRequest.DONE){
+
+                var status = Http.status;
+
+                if (status === 0 || (status >= 200 && status < 400)) {
+                    // The request has been completed successfully
+                    like_statistics = JSON.parse(Http.responseText);
+
+                    resolve({"like_statistics":like_statistics,
+                            "question2_index": question2_index})
+                }
+            }
+        }
+    })
     
-    ratio = parseInt(values['likes']) / parseInt(values['dislikes'])
+}
+function get_likes_dislikes_dom_element(values){
 
-    p = document.createElement('p');
-    p.innerHTML = ratio.toFixed(2);
+    ratio_td = document.createElement('td');
+    ratio = parseInt(values['like']) / parseInt(values['dislike'])
+    ratio_p = document.createElement('p');
+    ratio_p.className = "ratio_value";
+    ratio_p.innerHTML = ratio.toFixed(2);
+    ratio_td.appendChild(ratio_p)
 
-    td.appendChild(p)
-    return td
+
+    like_td = document.createElement('td');
+    like_p = document.createElement('p');
+    like_p.className = "like_value";
+    like_p.innerHTML = values['like'];
+    like_td.appendChild(like_p);
+
+    dislike_td = document.createElement('td');
+    dislike_p = document.createElement('p');
+    dislike_p.className = "dislike_value";
+    dislike_p.innerHTML = values['dislike'];
+    dislike_td.appendChild(dislike_p);
+
+    return [like_td, dislike_td, ratio_td];
 }
 
 function get_additional_button(text){
